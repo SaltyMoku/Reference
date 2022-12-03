e90f31d4d44bc7007e6ecf9b015adc544afdbbc0
SaltyMoku <francesco.sirio.monti@gmail.com>
2022-12-03 15:52:14 +0100

    Added AoC day 2

---
+++ aoc_2022/aoc_day2/aoc_day2_part_2.py
@@ -0,0 +1,36 @@
+# A - X - 1 Rock
+# B - Y - 2 Paper
+# C - Z - 3 Scissors
+# 0 - Lost
+# 3 - Draw
+# 6 - Win
+
+your_score = 0
+for line in (open("input.txt")):
+    line.split(" ")
+
+    if line[2] == "X":
+        if line[0] == "A":
+            your_score = your_score + 0 + 3
+        elif line[0] == "B":
+            your_score = your_score + 0 + 1
+        elif line[0] == "C":
+            your_score = your_score + 0 + 2
+        
+    if line[2] == "Y":
+        if line[0] == "A":
+            your_score = your_score + 3 + 1
+        elif line[0] == "B":
+            your_score = your_score + 3 + 2
+        elif line[0] == "C":
+            your_score = your_score + 3 + 3
+
+    if line[2] == "Z":
+        if line[0] == "A":
+            your_score = your_score + 6 + 2
+        elif line[0] == "B":
+            your_score = your_score + 6 + 3
+        elif line[0] == "C":
+            your_score = your_score + 6 + 1
+
+print("Your score is: " + str(your_score))
